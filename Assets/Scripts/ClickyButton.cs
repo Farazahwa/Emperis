4474ae56c00467eb310f6737aa43432ddb0c2a15
Mihazae <92701520+Farazahwa@users.noreply.github.com>
2022-10-03 14:06:32 +0700

    Scene start but 50%

---
+++ Assets/Scripts/ClickyButton.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+//Interface to implement if you wish to receive OnPointerDown callbacks.
+//Detects ongoing mouse clicks until release of the mouse button. Use IPointerUpHandler to handle the release of the mouse button.
+
+//Antarmuka untuk diterapkan jika Anda ingin menerima panggilan balik OnPointerDown.
+//Mendeteksi klik mouse yang sedang berlangsung hingga tombol mouse dilepaskan. 
+//Gunakan IPointerUpHandler untuk menangani pelepasan tombol mouse.
+
+public class ClickyButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private Image _img;
+    [SerializeField] private Sprite _default, _pressed;
+    [SerializeField] private AudioClip _compressClip, _uncompressClip;
+    [SerializeField] private AudioSource _source;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _img.sprite = _pressed;
+        _source.PlayOneShot(_compressClip);   
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _img.sprite = _default;
+        _source.PlayOneShot(_uncompressClip);
+    }
+
+    public void WasClicked()
+    {
+        Debug.Log("Click!");
+    }
+}
