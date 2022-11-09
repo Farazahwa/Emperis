1328888a3ab104be08eabc4d9459b42c3adc18ca
Husniadyaksa <husniadyaksa112@gmail.com>
2022-11-09 09:42:45 +0700

    Edit Script To move Cave Scene

--- Assets/Scripts/PortalController.cs
+++ Assets/Scripts/PortalController.cs
@@ -5,6 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class PortalController : MonoBehaviour
 {
+   void OnTriggerEnter2D(Collider2D col)
+   {
+      Debug.Log("OnTriggerEnter");
+      if (col.CompareTag("Player"))
+      {
+         CompleteLevel();
+      }
+   }
+
    private void CompleteLevel()
    {
     SceneManager.LoadScene("Cave Scene");
